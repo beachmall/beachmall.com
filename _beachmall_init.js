51b64a24d042c2d04bcc45b792e6e584863edab5
ryanm <ryanm@zoovy.com>
2013-06-24 11:09:40 -0700

    disabled unused extension

--- _beachmall_init.js
+++ _beachmall_init.js
@@ -18,7 +18,7 @@ app.rq.push(['extension',0,'beachmart','extensions/beachmart_custom.js']); // cu
 app.rq.push(['extension',0,'store_filter','extensions/beachmart.js','startExtension']); 
 app.rq.push(['extension',0,'beachmart_dropdown','extensions/beachmart_dropdown.js', 'startExtension']); // custom product getter for dropdowns Runs startExtension as well
 app.rq.push(['extension',0,'beachmart_catsearch','extensions/beachmart_catsearch.js', 'startExtension']); // custom search of designated category and tag
-app.rq.push(['extension',0,'beachmart_items_per_page','extensions/beachmart_items_per_page.js', 'startExtension']); // changes number of items displayed in current prodlist
+//app.rq.push(['extension',0,'beachmart_items_per_page','extensions/beachmart_items_per_page.js', 'startExtension']); // changes number of items displayed in current prodlist
 app.rq.push(['extension',0,'beachmart_banner','extensions/beachmart_banner.js']); // add banner to homepage that can be changed in _banners.json file 
 app.rq.push(['extension',0,'beachmart_dropdown_image','extensions/beachmart_dropdown_image.js']); // add banner to homepage that can be changed in _banners.json file 
 
