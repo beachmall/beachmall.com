07b88320b09823d24200d19f5f28e12af0bec74f
ryanm <ryanm@zoovy.com>
2013-06-28 15:11:18 -0700

    Previously viewed carousel scroll set to 4

--- extensions/beachmart.js
+++ extensions/beachmart.js
@@ -876,7 +876,7 @@ return filters;
 									width: 960,
 									//items: 1,
 									pagination: '.productPreviousViewedPagenation',
-									scroll: 1,
+									scroll: 4,
 							//		mousewheel: true, //this is mobile, so mousewheel isn't necessary (plugin is not loaded)
 									swipe: {
 										onMouse: true,
