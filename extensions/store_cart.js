7257d8bc72cf94eb2b87ef0a0c3b9bd2aaac5204
ryanm <ryanm@zoovy.com>
2013-10-31 16:27:21 -0700

    Cart estimated arrival work, incomplete at best

--- extensions/store_cart.js
+++ extensions/store_cart.js
@@ -364,11 +364,21 @@ $tag.one('click',function(event){
 					if(isSelectedMethod)
 						o += " checked='checked' "
 /*BEACHMART*/		if(data.value[i].amount == 0) {		
-/*BEACHMALL*/			o += "/>"+shipName+": <span class='cartFree' >FREE<\/span><\/label><\/li>";
+/*BEACHMALL*/			o += "/>"+shipName+": <span class='cartFree' >FREE<\/span>";
 /*BEACHMALL*/			}
 /*BEACHMALL*/		else {
-/*BEACHMART*/			o += "/>"+shipName+": <span >"+app.u.formatMoney(data.value[i].amount,'$','',false)+"<\/span><\/label><\/li>";
+/*BEACHMART*/			o += "/>"+shipName+": <span >"+app.u.formatMoney(data.value[i].amount,'$','',false)+"<\/span>";
 /*BEACHMART*/			}
+//new ish that may need to come out..........................................................
+					var d = new Date();
+					var arriveMonth = d.getMonth() + 1;
+					var arriveDay = Number(d.getDay());
+					var guaranteedDay = Number(data.value[i].guaranteed_delivery_days);
+					app.u.dump('now date'); app.u.dump(arriveMonth+"/"+arriveDay);
+					arriveDay = arriveDay+guaranteedDay;
+					app.u.dump('arrive date'); app.u.dump(arriveMonth+"/"+arriveDay);
+					o += "<span class='shippingArriveDate'> (arrival on "+arriveMonth+"/"+arriveDay+"<\/span><\/label><\/li>";
+//new ish that may need to come out..........................................................				
 				}
 				$tag.html(o);
 				} //shipMethodsAsRadioButtons
