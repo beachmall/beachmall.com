a3c45b43208d8f6d617962260e85984ee70b92de
rmurray <ryanm@zoovy.com>
2014-04-14 22:28:59 -0700

    cart e-mail callback message / hiding
    Set anymessage to display success or failure, and to hide form after 10
    secs.

--- extensions/beachmall_cartEmail.js
+++ extensions/beachmall_cartEmail.js
@@ -28,21 +28,7 @@ var beachmall_cartemail = function(_app) {
 ////////////////////////////////////   CALLS    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\	
 	
 	calls : {	
-	
-		cartemailmashup: {
-			init : function(params,_tag,Q)	{
-				this.dispatch(params,_tag,Q);
-			},
-			dispatch : function(params,_tag,Q)	{
-				_app.model.addDispatchToQ( $.extend(true,{
-				    '_cmd':'appMashUpRedis',
-					'_tag':_tag,
-				    }, params) , 'immutable');
-				
-			dump('---Q'); dump(Q);
-			}			
-		}, //cartemailmashup
-	
+		
 	}, //calls
 	
 ////////////////////////////////////   CALLBACKS    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -78,63 +64,38 @@ var beachmall_cartemail = function(_app) {
 //these are going the way of the do do, in favor of app events. new extensions should have few (if any) actions.
 		a : {
 		
-				//processes cart e-mail form and calls appMashUpSMTP for it
+				//processes cart e-mail form and calls appMashUpRedis for it
 			emailcart : function($form) {
-			dump('-----start email cart...');
+//			dump('-----start email cart...');
 				var uName = $('input[type="text"]',$form).val();
 				var eAddress = $('input[type="email"]',$form).val();
 				var newsletter = $('input[type="checkbox"]',$form).is(':checked');
-				var cartID = _app.model.fetchCartID();
-				var products = _app.data['cartDetail|'+cartID]['@ITEMS'];
-				var body = "This is the contents of your Beachmall.com cart. It was e-mailed to you at your request. "
-					+		"Please come see us again soon!"; 
-				
 				var params = {
 					'_cartid' 	: _app.model.fetchCartID(),
 					'platform' 	: 'appMashUpRedis-EMAILCART.json',
-					'%vars' : {
-						'email':eAddress,
-						'fullname':uName
-					    }
-				//	"permission"	: "_mashups/cartEmailPermissions",
-				//	"sender"		: "help@beachmall.com",
-				//	"recipient"		: eAddress,
-				//	"subject"		: "Your Beachmall.com cart contents",
-				//	"products"		: products,
-				//	"body"			: body
+					'%vars' 	: {
+								  'email':eAddress,
+								  'fullname':uName
+					},
+					'_cmd'		: 'appMashUpRedis',
+					'_tag'		: {
+						'callback':function(rd){
+							if(_app.model.responseHasErrors(rd)) {
+								$form.anymessage({'message':rd});
+							}
+							else {
+								$form.anymessage(_app.u.successMsgObject('Your message has been sent.'));
+								setTimeout(function(){_app.ext.beachmall_cartemail.a.hideCartEmail($('span',$form),$form);},10000);
+//								_gaq.push(['_trackEvent','Cart','User Event','Cart e-mailed']);
+							}
+						}
+					}
 				};
 
-				params._cmd = 'appMashUpRedis';
 				_app.model.addDispatchToQ(params,'immutable');
 				_app.model.dispatchThis('immutable');
-				
-				 //_app.ext.beachmall_cartemail.calls.cartemailmashup.init(params,{"callback":function(rd){				
-				//	if(_app.model.responseHasErrors(rd)){
-				//		$form.anymessage({'message':rd});
-				//	}
-				//	else	{
-				//		$form.anymessage(_app.u.successMsgObject('Your message has been sent.'));
-//				//			_gaq.push(['_trackEvent','Cart','User Event','Cart e-mailed']);
-				//	}
-				//}});
-			
-		
-		//		_app.u.dump('--> All that stuff from the e-mail form:'); 
-		//		_app.u.dump(uName); 
-		//		_app.u.dump(eAddress); 
-		//		_app.u.dump(newsletter);
-		//		_app.u.dump(products);
-		//		_app.u.dump(params);
-		//		_app.u.dump(body);
-	
-			//HIDDEN FOR EASE OF TESTING, UNCOMMENT WHEN DONE
-			//	if(newsletter) {
-			//		_app.ext.store_crm.u.handleSubscribe($form);
-			//	}
-			//	setTimeout(function() {
-			//		_app.ext.beachmall_cartEmail.a.hideCartEmail($('span',$form),$form);
-			//	},5000);
-			},
+
+			}, //emailcart
 		
 				//animates cart e-mail form into view, shows close button in form
 			showCartEmail : function($this, $form) {
@@ -178,81 +139,7 @@ var beachmall_cartemail = function(_app) {
 //utilities are typically functions that are exected by an event or action.
 //any functions that are recycled should be here.
 		u : {
-		
-				//text function to call from console. Call w/:
-				// _app.ext.beachmall_cartEmail.u.runMashupTest($('form.noHover','#modalCart'));
-			runMashupTest : function($form) {
-				var uName = $('input[type="text"]',$form).val();
-				var eAddress = $('input[type="email"]',$form).val();
-				var newsletter = $('input[type="checkbox"]',$form).is(':checked');
-				var cartID = _app.model.fetchCartID();
-				var products = _app.data['cartDetail|'+cartID]['@ITEMS'];
-				var body = "Something to see as the e-mail body";
-		/*		var products = [
-				{
-					"sku": "BCHCDY",
-					"%attribs": {
-						"zoovy:profile": "DEFAULT",
-						"zoovy:pkg_width": "15",
-						"zoovy:prod_mfg": "Bcaddy",
-						"zoovy:pkg_height": "38",
-						"zoovy:prod_mfgid": "GENERIC:BHCADY",
-						"zoovy:ship_cost1": "0.0",
-						"zoovy:virtual": "GENERIC:BHCADY",
-						"zoovy:prod_image1": "beach_caddy/haulincart2_4.jpg",
-						"zoovy:prod_supplier": "BHCADY",
-						"zoovy:pkg_depth": "22",
-						"is:shipfree": "1",
-						"zoovy:prod_is": "1073153",
-						"zoovy:prod_promoclass": "FREE SHIP",
-						"zoovy:ship_latency": "1",
-						"zoovy:pkg_exclusive": "1"
-					},
-					"uuid": "FD8C6214578611E3982BA3A338230745",
-					"shipdsn": "",
-					"weight": "528",
-					"base_price": "349.00",
-					"assembly": "",
-					"prod_name": "Wide Wheels Utility Cart with Tote(included), and Surf Board Option",
-					"pog_sequence": "",
-					"taxable": "1",
-					"qty": "1",
-					"description": "Wide Wheels Utility Cart with Tote(included), and Surf Board Option",
-					"base_weight": "528",
-					"virtual": "GENERIC:BHCADY",
-					"full_product": {
-						
-					},
-					"added_gmt": "1385572304",
-					"product": "BCHCDY",
-					"price": "349.00",
-					"stid": "BCHCDY",
-					"extended": "349.00"
-				}
-				]
-		*/		
-				var params = {
-					"permission"	: "_mashups/cartEmailPermissions",
-					"sender"		: "help@beachmall.com",
-					"recipient"		: "ryanm@zoovy.com",
-					"subject"		: "Your Beachmall.com cart contents",
-					"products"		: products,
-					"body"			: "Something to see as the e-mail body"
-				};
 				
-				//_app.u.dump('--> PARAMS'); _app.u.dump(params);
-				
-		/*		_app.ext.beachmall_cartemail.calls.cartemailmashup.init(params,{"callback":function(rd){
-					if(_app.model.responseHasErrors(rd)){
-						$form.anymessage({'message':rd});
-					}
-					else	{
-						$form.anymessage(_app.u.successMsgObject('Your message has been sent.'));
-//							_gaq.push(['_trackEvent','Cart','User Event','Cart e-mailed']);
-					}
-				}});
-		*/	},
-		
 		}, //u [utilities]
 
 //app-events are added to an element through data-app-event="extensionName|functionName"
