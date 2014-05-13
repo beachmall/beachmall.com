347008b313ad422fbecc918a15b76299fd46f225
rmurray <ryanm@zoovy.com>
2014-05-13 16:10:15 -0700

    remove apptimizer from analytics / version
    moved analytics extension call back to init.

--- _beachmall_init.js
+++ _beachmall_init.js
@@ -46,7 +46,7 @@ myApp.rq.push(['extension',0,'beachmall_recentlyviewed','','startExtension']); /
 //myApp.rq.push(['script',0,myApp.vars.baseURL+'resources/jquery.touchSwipe-1.3.3.min.js']); //used w/ carouFedSel.
 //myApp.rq.push(['script',0,myApp.vars.baseURL+'resources/jquery.carouFredSel-6.2.0.min.js']); //used for carousels.
 
-myApp.rq.push(['extension',0,'google_analytics','','startExtension']);
+myApp.rq.push(['extension',0,'google_analytics','extensions/partner_google_analytics.js','startExtension']);
 myApp.rq.push(['extension',0,'google_adwords','extensions/partner_google_adwords.js','startExtension']);
 //myApp.rq.push(['extension',1,'tools_ab_testing','extensions/tools_ab_testing.js']);
 myApp.rq.push(['extension',0,'partner_addthis','extensions/partner_addthis.js','startExtension']);
