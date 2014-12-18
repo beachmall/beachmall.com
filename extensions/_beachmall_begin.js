f9307f23736322d5dd410e2552ec3deaeb1daa9d
rmurray <ryanm@zoovy.com>
2014-12-18 15:08:38 -0800

    Revert "magiczoom defered css/js loading / version"
    This reverts commit ad7f6d91ef65ac989b4aec6e8c1e2ed76793e7d6.

--- extensions/_beachmall_begin.js
+++ extensions/_beachmall_begin.js
@@ -36,11 +36,9 @@ var beachmall_begin = function(_app) {
 			onSuccess : function()	{
 				var r = true; //return false if extension won't load for some reason (account config, dependencies, etc).
 				
-				setTimeout(function(){
-					_app.u.loadResourceFile(['script',0,'magiczoomplus-commercial/magiczoomplus/magiczoomplus.js',function(){
-						MagicZoomPlus.start();
-					}]);
-				},0);
+				_app.u.loadResourceFile(['script',0,'magiczoomplus-commercial/magiczoomplus/magiczoomplus.js',function(){
+					MagicZoomPlus.start();
+				}]);
 					
 				return r;
 			},
